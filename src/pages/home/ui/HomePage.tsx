57f74612b4f309cdc059bb7a7150264ced1164f2
JohnnyLU <ctygholog304@gmail.com>
2026-04-02 22:19:23 +0500

    feat: add devices section widget

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,19 +1,19 @@
 import {HeroBanner} from "../../../widgets/hero-banner";
-// import {GenresSection} from "../../../widgets/genres-section";
-// import {DevicesSection} from "../../../widgets/devices-section";
-// import {FaqSection} from "../../../widgets/faq-section";
-// import {PricingSection} from "../../../widgets/pricing-section";
-// import {FreeTrialBanner} from "../../../widgets/free-trial-banner";
+ import {GenresSection} from "../../../widgets/genres-section";
+import {DevicesSection} from "../../../widgets/devices-section";
+import {FaqSection} from "../../../widgets/faq-section";
+import {PricingSection} from "../../../widgets/pricing-section";
+import {FreeTrialBanner} from "../../../widgets/free-trial-banner";
 
 export function HomePage() {
   return (
     <>
       <HeroBanner />
-{/*      <GenresSection />
+      <GenresSection />
       <DevicesSection />
       <FaqSection />
       <PricingSection />
-      <FreeTrialBanner />*/}
+      <FreeTrialBanner />
     </>
   )
 }
--- src/widgets/devices-section/ui/DevicesSection.tsx
+++ src/widgets/devices-section/ui/DevicesSection.tsx
@@ -1,7 +1,36 @@
+import styles from "./DevicesSection.module.scss";
+import {Container} from "../../../shared/ui/container/Container";
+import {devices} from "../lib/devices.data";
+
 export function DevicesSection () {
   return (
     <section>
-      <h2>DevicesSection</h2>
+      <Container>
+        <div className={styles.inner}>
+          <div className={styles.header}>
+            <h2 className={styles.heading}>
+              We Provide you streaming experience across various devices.
+            </h2>
+            <p className={styles.subtext}>With StreamVibe, you can enjoy your favorite movies and TV shows anytime, anywhere. Our platform is designed to be compatible with a wide range of devices, ensuring that you never miss a moment of entertainment.</p>
+          </div>
+
+          <div className={styles.cards}>
+            <ul className={styles.list}>
+              {devices.map(item => (
+                <li key={item.title} className={styles.card}>
+                  <div className={styles.top}>
+                    <div className={styles.iconWrapper}>
+                      <item.icon className={styles.cardIcon}/>
+                    </div>
+                    <h3 className={styles.cardTitle}>{item.title}</h3>
+                  </div>
+                  <p className={styles.cardDescription}>{item.description}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </Container>
     </section>
   )
 }
