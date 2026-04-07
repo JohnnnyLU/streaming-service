bd22bb068adc8384365ef8d0bcb7843c55f1a27c
JohnnyLU <ctygholog304@gmail.com>
2026-04-07 10:34:40 +0500

    feat: add free trial banner widget

--- src/widgets/free-trial-banner/ui/FreeTrialBanner.tsx
+++ src/widgets/free-trial-banner/ui/FreeTrialBanner.tsx
@@ -1,7 +1,21 @@
+import {Container} from "../../../shared/ui/container/Container.tsx";
+import styles from "./FreeTrialBanner.module.scss";
+import {Button} from "../../../shared/ui/button/Button.tsx";
+
 export function FreeTrialBanner() {
   return (
-    <section>
-      <h2>FreeTrialBanner</h2>
+    <section className={styles.trialBanner}>
+      <Container>
+        <div className={styles.inner}>
+          <div className={styles.header}>
+            <h2 className={styles.title}>Start your free trial today!</h2>
+            <p className={styles.subtext}>This is a clear and concise call to action that encourages users to sign up for a free trial of StreamVibe.</p>
+          </div>
+          <div>
+            <Button children="Start a Free Trial" />
+          </div>
+        </div>
+      </Container>
     </section>
   )
 }
